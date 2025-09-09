e7e010758e9f96e7ca167c5b2b11a491b04e1b30
Pablo <civernet@gmail.com>
2025-09-09 13:29:10 -0600

    Agrgando vistas de Clientes

--- app/Orchid/Screens/CustomerEditScreen.php
+++ app/Orchid/Screens/CustomerEditScreen.php
@@ -3,8 +3,14 @@
 namespace App\Orchid\Screens;
 
 use App\Models\Customer;
+use Illuminate\Http\Request;
 use Orchid\Screen\Actions\Button;
+use Orchid\Screen\Fields\Group;
+use Orchid\Screen\Fields\Input;
 use Orchid\Screen\Screen;
+use Orchid\Support\Facades\Alert;
+use Orchid\Support\Facades\Layout;
+use Illuminate\Support\Facades\Http;
 
 class CustomerEditScreen extends Screen
 {
@@ -53,13 +59,15 @@ class CustomerEditScreen extends Screen
                 ->method('createOrUpdate')
                 ->canSee(!$this->customer->exists),
 
-            Button::make('Actualizar Cliente')
-                ->icon('note')
+            Button::make('Actualizar')
+                ->icon('pencil')
+                ->class('btn btn-info')
                 ->method('createOrUpdate')
                 ->canSee($this->customer->exists),
 
-            Button::make('Eliminar Cliente')
+            Button::make('Eliminar')
                 ->icon('trash')
+                ->class('btn btn-danger')
                 ->method('remove')
                 ->canSee($this->customer->exists),
         ];
@@ -73,7 +81,95 @@ class CustomerEditScreen extends Screen
     public function layout(): iterable
     {
         return [
+            Layout::rows([
+                Button::make('Consulta Nit')
+                    ->icon('magnifier')
+                    ->method('consultaNit')
+                    ->class('btn btn-warning')
+                    ->style('margin-top: 30px; margin-bottom: 0px;'),
+                Group::make([
 
+                    Input::make('customer.nit')
+                        ->title('NIT')
+                        ->placeholder('NIT del cliente')
+                        ->required(),
+
+                    Input::make('customer.name')
+                        ->title('Nombre')
+                        ->placeholder('Nombre del cliente')
+                        ->required(),
+
+                ])->widthColumns('1fr 1fr 2fr'),
+
+                Input::make('customer.email')
+                    ->title('Email')
+                    ->placeholder('Email del cliente'),
+
+                Input::make('customer.phone')
+                    ->title('Teléfono')
+                    ->placeholder('Teléfono del cliente'),
+
+                Input::make('customer.address')
+                    ->title('Dirección')
+                    ->placeholder('Dirección del cliente'),
+
+                Input::make('customer.phone2')
+                    ->title('Teléfono 2')
+                    ->placeholder('Segundo teléfono del cliente'),
+            ])
         ];
+    }
+
+    /**
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function createOrUpdate(Request $request)
+    {
+        $this->customer->fill($request->get('customer'))->save();
+
+        Alert::info('You have successfully created a post.');
+
+        return redirect()->route('platform.customer.list');
+    }
+
+    /**
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function remove()
+    {
+        $this->customer->delete();
+
+        Alert::info('You have successfully deleted the post.');
+
+        return redirect()->route('platform.customer.list');
+    }
+
+    public function consultaNit(Request $request)
+    {
+        $nit = $request->get('customer')['nit'];
+
+        $response = Http::withHeaders([
+            'Authorization' => 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IkdULjAwMDA0NDY1Mzk0OC5DSVZFUk5FVFRFU1QiLCJuYmYiOjE3NTc0NDAxMDgsImV4cCI6MTc4ODU0NDEwOCwiaWF0IjoxNzU3NDQwMTA4LCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjQ5MjIwIiwiYXVkIjoiaHR0cDovL2xvY2FsaG9zdDo0OTIyMCJ9.kOBx0bE3pkj0YTZzFmTPtgUysPckGm7_GFaSjIY3hWI',
+            'Content-Type'  => 'application/json',
+        ])->get('https://felgttestaws.digifact.com.gt/gt.com.fel.api.v3/api/sharedInfo', [
+            'NIT'      => '44653948',
+            'DATA1'    => 'SHARED_GETINFONITcom',
+            'DATA2'    => 'NIT|54731747',
+            'USERNAME' => 'CIVERNETTEST',
+        ]);
+        $data = $response->json();
+
+        $name = $data['RESPONSE'][0]['NOMBRE'] ?? '';
+
+        Alert::info('Consulta realizada, el nombre es: ' . $name);
+
+        return redirect()->back()->withInput([
+            'customer' => [
+                'nit' => $nit,
+                'name' => $name
+            ]
+        ]);
     }
 }
