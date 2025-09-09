42c9ca2e82abdcb7750437bd9a1e618c40dee327
Pablo <civernet@gmail.com>
2025-09-09 13:29:38 -0600

    Agrgando vistas de Proveedores

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Orchid\Screen\AsSource;
 
 class Supplier extends Model
 {
-    //
+    use AsSource;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'address',
+        'phone2',
+        'nit'
+    ];
 }
