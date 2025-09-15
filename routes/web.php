1eef51fe4318c5dd94ea80688884953e85cb7eac
Pablo <civernet@gmail.com>
2025-09-14 22:09:23 -0600

    Corrigiendo el formulario de facturas de venta, usando orchid

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
+
 <?php
 
 use Illuminate\Support\Facades\Route;
 
+use App\Models\Product;
+
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/api/product/{id}', function ($id) {
+    $product = Product::find($id);
+    if (!$product) {
+        return response()->json(['error' => 'Producto no encontrado'], 404);
+    }
+    return response()->json([
+        'reference'   => $product->reference,
+        'description' => $product->description,
+        'price'       => $product->price,
+        'cost'        => $product->cost,
+    ]);
 });
